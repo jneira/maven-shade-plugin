cfd6bf876fae830a79955fb7bfbb30dedb4b0221
jneira <atreyu.bbb@gmail.com>
2023-02-26 23:01:44 +0100

    Correct out

--- src/it/projects/MSHADE-345_scala-package-relocation/src/main/scala/MyApp.scala
+++ src/it/projects/MSHADE-345_scala-package-relocation/src/main/scala/MyApp.scala
@@ -2,6 +2,6 @@ import org.apache.logging.log4j.scala.Logger;
 import org.apache.logging.log4j.scala.Logging;
 
 object MyApp extends App with Logging {
-  println("Logger FQCN" + Logger.FQCN)
+  println("Logger FQCN: " + Logger.FQCN)
   logger.info("Test Logging")
 }
